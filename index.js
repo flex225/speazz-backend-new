c856f3dcccd05e62b75fa25c2a72edc44a13d544
flex225 <aakhnoyan@gmail.com>
2021-09-06 19:54:44 +0400

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var logger = require('morgan')
+var cors = require('cors')
 var indexRouter = require('./routes')
 var app = express()
 
@@ -8,6 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/', indexRouter)
 
+app.use(cors())
 
 app.use(function (err, req, res, next) {
   res.locals.message = err.message
