4957ff2f54b35704e227301ddd3e4ca0fbf1431d
flex225 <aakhnoyan@gmail.com>
2021-09-06 19:58:37 +0400

    cors

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ var cors = require('cors')
 var indexRouter = require('./routes')
 var app = express()
 
+app.use(cors())
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/', indexRouter)
 
-app.use(cors())
 
 app.use(function (err, req, res, next) {
   res.locals.message = err.message
