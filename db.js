922cef964cb1101973d18d475e87c5333199c7e0
flex225 <aakhnoyan@gmail.com>
2021-09-06 04:12:01 +0400

    mysql

--- db.js
+++ db.js
@@ -19,7 +19,28 @@ const mysql = require('mysql')
 
 const client = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
 
-client.connect();
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+                                                
+
+  connection.connect(function(err) {             
+    if(err) {                                    
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }                                    
+  });                                    
+                                        
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();                        
+    } else {                                     
+      throw err;                                 
+    }
+  });
+}
+
+handleDisconnect();
 
 console.log('Connected to database')
 
